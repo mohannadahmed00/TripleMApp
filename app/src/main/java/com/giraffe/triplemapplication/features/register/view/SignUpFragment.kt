e1ad5cdb0ab5235cdcc9cb859f0ad963d5e3721d
MuhammedEhabAND <muhammedehab726@gmail.com>
2023-11-17 14:50:43 +0200

    sign up screen design

---
+++ app/src/main/java/com/giraffe/triplemapplication/features/register/view/SignUpFragment.kt
@@ -0,0 +1,31 @@
+package com.giraffe.triplemapplication.features.register.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.giraffe.triplemapplication.R
+import com.giraffe.triplemapplication.bases.BaseFragment
+import com.giraffe.triplemapplication.databinding.FragmentProfileBinding
+import com.giraffe.triplemapplication.features.profile.viewmodel.ProfileVM
+
+
+class SignUpFragment : BaseFragment<ProfileVM, FragmentProfileBinding>() {
+    override fun getViewModel(): Class<ProfileVM> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        b: Boolean,
+    ): FragmentProfileBinding {
+        TODO("Not yet implemented")
+    }
+
+    override fun handleView() {
+        TODO("Not yet implemented")
+    }
+
+
+}
