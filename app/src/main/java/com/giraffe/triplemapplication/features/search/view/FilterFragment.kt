b7f0e064bb1b64c2af1ae3783c0ff72eb84ecbf7
MuhammedEhabAND <muhammedehab726@gmail.com>
2023-11-18 02:53:37 +0200

    adding the main search screen design

---
+++ app/src/main/java/com/giraffe/triplemapplication/features/search/view/FilterFragment.kt
@@ -0,0 +1,29 @@
+package com.giraffe.triplemapplication.features.search.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.giraffe.triplemapplication.R
+import com.giraffe.triplemapplication.bases.BaseFragment
+import com.giraffe.triplemapplication.databinding.FragmentSearchBinding
+import com.giraffe.triplemapplication.features.search.viewmodel.SearchVM
+
+class FilterFragment :  BaseFragment<SearchVM, FragmentSearchBinding>() {
+    override fun getViewModel(): Class<SearchVM> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        b: Boolean,
+    ): FragmentSearchBinding {
+        TODO("Not yet implemented")
+    }
+
+    override fun handleView() {
+        TODO("Not yet implemented")
+    }
+}
