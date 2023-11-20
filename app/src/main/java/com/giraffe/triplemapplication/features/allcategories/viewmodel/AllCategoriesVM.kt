3acb8b587e93fc88cf60869b9833c0387ce12f00
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2023-11-20 20:06:35 +0200

    Safe call

--- app/src/main/java/com/giraffe/triplemapplication/features/allcategories/viewmodel/AllCategoriesVM.kt
+++ app/src/main/java/com/giraffe/triplemapplication/features/allcategories/viewmodel/AllCategoriesVM.kt
@@ -8,6 +8,7 @@ import com.giraffe.triplemapplication.model.categories.AllCategoriesResponse
 import com.giraffe.triplemapplication.model.products.AllProductsResponse
 import com.giraffe.triplemapplication.model.repo.RepoInterface
 import com.giraffe.triplemapplication.utils.Resource
+import com.giraffe.triplemapplication.utils.safeCall
 import com.google.gson.JsonSyntaxException
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
@@ -35,50 +36,13 @@ class AllCategoriesVM(private val repo: RepoInterface): ViewModel() {
 
     private fun getAllCategories() {
         viewModelScope.launch {
-            _allCategoriesFlow.emit(safeApiCalls { repo.getAllCategories() })
+            _allCategoriesFlow.emit(safeCall { repo.getAllCategories() })
         }
     }
 
     private fun getAllBrands() {
         viewModelScope.launch {
-            _allBrandsFlow.emit(safeApiCalls { repo.getAllBrands() })
-        }
-    }
-
-    private suspend fun <T> safeApiCalls(apiCall: suspend () -> Flow<T>): Resource<T> {
-        return withContext(Dispatchers.IO) {
-            var resource:Resource<T> = Resource.Loading
-            try {
-                apiCall.invoke()
-                    .catch {
-                        resource = Resource.Failure(true,0,it)
-                    }.collect{
-                        resource = Resource.Success(it)
-                    }
-            }catch (throwable: Throwable){
-                when (throwable) {
-                    is HttpException -> {
-                        Log.e("BaseRepoHttpErrorBody->","${throwable.response()!!.errorBody()}")
-                        resource = Resource.Failure(
-                            false,
-                            throwable.code(),
-                            throwable.response()!!.errorBody() as ResponseBody
-                        )
-                    }
-                    is JsonSyntaxException -> {
-                        Log.e("BaseRepoJsonErrorBody->", throwable.localizedMessage)
-                        resource = Resource.Failure(
-                            false,
-                            0,
-                            throwable.localizedMessage
-                        )
-                    }
-                    else -> {
-                        resource = Resource.Failure(true, null, null)
-                    }
-                }
-            }
-            resource
+            _allBrandsFlow.emit(safeCall { repo.getAllBrands() })
         }
     }
 }
