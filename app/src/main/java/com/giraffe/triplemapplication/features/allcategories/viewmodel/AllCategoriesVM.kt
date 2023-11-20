da6f4d70389922c206f04ae2584f59603de229c5
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2023-11-20 20:06:57 +0200

    Safe call

--- app/src/main/java/com/giraffe/triplemapplication/features/allcategories/viewmodel/AllCategoriesVM.kt
+++ app/src/main/java/com/giraffe/triplemapplication/features/allcategories/viewmodel/AllCategoriesVM.kt
@@ -1,25 +1,16 @@
 package com.giraffe.triplemapplication.features.allcategories.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.giraffe.triplemapplication.model.brands.AllBrandsResponse
 import com.giraffe.triplemapplication.model.categories.AllCategoriesResponse
-import com.giraffe.triplemapplication.model.products.AllProductsResponse
 import com.giraffe.triplemapplication.model.repo.RepoInterface
 import com.giraffe.triplemapplication.utils.Resource
 import com.giraffe.triplemapplication.utils.safeCall
-import com.google.gson.JsonSyntaxException
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import okhttp3.ResponseBody
-import retrofit2.HttpException
 
 class AllCategoriesVM(private val repo: RepoInterface): ViewModel() {
 
