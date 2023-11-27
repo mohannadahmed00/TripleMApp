d67f5693abe6a55bf6e98a54543ab2f243a57d4a
MuhammedEhabAND <muhammedehab726@gmail.com>
2023-11-27 08:38:45 +0200

    fix the problem of set cache cart id null if there is a cart id on firebase

--- app/src/main/java/com/giraffe/triplemapplication/features/login/viewmodel/LoginVM.kt
+++ app/src/main/java/com/giraffe/triplemapplication/features/login/viewmodel/LoginVM.kt
@@ -18,17 +18,17 @@ import kotlinx.coroutines.launch
 
 class LoginVM(private val repo: RepoInterface) : ViewModel() {
 
+    private var cartId: Long? = null
+    private var wishListId: Long? = null
     private val _firebaseUser: MutableStateFlow<Resource<AuthResult>> =
         MutableStateFlow(Resource.Loading)
     val currentUser: StateFlow<Resource<AuthResult>> = _firebaseUser.asStateFlow()
+    private val _wishListId : MutableStateFlow<Resource<Long>> = MutableStateFlow(Resource.Loading)
 
-    //    private val _wishListId : MutableStateFlow<Resource<Long>> = MutableStateFlow(Resource.Loading)
-//    private val _cartId : MutableStateFlow<Resource<Long>> = MutableStateFlow(Resource.Loading)
-//    private val _customer : MutableStateFlow<Resource<CustomerResponse>> = MutableStateFlow(Resource.Loading)
-    private var wishListId: Long? = null
-    private var cartId: Long? = null
-    private val _customerId: MutableStateFlow<Long?> = MutableStateFlow(null)
-    val customerId: StateFlow<Long?> = _customerId.asStateFlow()
+    private val _cartId : MutableStateFlow<Resource<Long>> = MutableStateFlow(Resource.Loading)
+
+
+
     private val _customer: MutableStateFlow<Resource<MultipleCustomerResponse>> =
         MutableStateFlow(Resource.Loading)
     val customer : StateFlow<Resource<MultipleCustomerResponse>> = _customer
@@ -37,6 +37,50 @@ class LoginVM(private val repo: RepoInterface) : ViewModel() {
     fun login(email: String, password: String) {
         viewModelScope.launch(Dispatchers.IO) {
             _firebaseUser.emit(safeCall { repo.signInFirebase(email, password) })
+//            repo.uploadCustomerId()
+
+        }
+    }
+
+
+    private fun getWishListIdFromFirebase(){
+        viewModelScope.launch(Dispatchers.IO) {
+            _wishListId.emit(safeCall{repo.getWishListId()} )
+        }
+    }
+    private fun getCartIdFromFirebase(){
+        viewModelScope.launch(Dispatchers.IO) {
+            _cartId.emit(safeCall{repo.getCartId()} )
+        }
+    }
+    private fun collectCartId(){
+        viewModelScope.launch(Dispatchers.IO) {
+            _cartId.collectLatest {
+                when(it){
+                    is Resource.Failure -> {}
+                    Resource.Loading -> {
+
+                    }
+                    is Resource.Success -> {
+                        cartId = it.value
+                    }
+                }
+            }
+        }
+    }
+    private fun collectWishListId(){
+        viewModelScope.launch(Dispatchers.IO) {
+            _wishListId.collectLatest {
+                when(it){
+                    is Resource.Failure -> {}
+                    Resource.Loading -> {
+
+                    }
+                    is Resource.Success -> {
+                        wishListId = it.value
+                    }
+                }
+            }
         }
     }
 
@@ -44,11 +88,15 @@ class LoginVM(private val repo: RepoInterface) : ViewModel() {
 
     fun getCustomerByEmail(email : String){
         viewModelScope.launch(Dispatchers.IO){
+            getCartIdFromFirebase()
+            getWishListIdFromFirebase()
+            collectCartId()
+            collectWishListId()
             _customer.emit(safeCall { repo.getCustomerByEmail(email) })
         }
     }
 
-    fun setData(customerId : Long) {
+    fun setData(customerId : Long ) {
         viewModelScope.launch(Dispatchers.IO) {
             repo.setCustomerIdLocally(customerId)
             repo.setCartIdLocally(cartId)
